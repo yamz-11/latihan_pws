1175c795e68b570af899c75b070c0a5e0e2c80f4
yamz-11 <wiratamajrz@gmail.com>
2022-12-01 10:55:06 +0700

    latihan pws

---
+++ src/main/java/pws/latihan/demo/CApplication.java
@@ -0,0 +1,13 @@
+package pws.latihan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pws/latihan/demo/CApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.latihan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
